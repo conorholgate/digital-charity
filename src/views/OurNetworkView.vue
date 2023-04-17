9512a4767831d19dd1a482513e8963a0ba627609
Conor Holgate <conor.ianholgate@gmail.com>
2023-04-17 13:34:11 +0100

    added content on other pages

---
+++ src/views/OurNetworkView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem similique saepe iste quae, harum quidem est placeat nulla reiciendis rerum
+    aperiam voluptatem sint perspiciatis at doloribus repudiandae ab eveniet alias!
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'our-network',
+}
+</script>
+
+<style lang="scss" scoped></style>
